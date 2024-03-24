906c2e4cab34990246e5b9f9ff215f05c46a486c
Pranav M <pranavmappoli@gmail.com>
2024-03-25 01:51:11 +0530

    add src folder

---
+++ src/app/submit/[formUrl]/page.tsx
@@ -0,0 +1,24 @@
+import { GetFormContentByUrl } from "@/src/actions/form";
+import { FormElementInstance } from "@/src/components/FormElements";
+import FormSubmitComponent from "@/src/components/FormSubmitComponent";
+import React from "react";
+
+async function SubmitPage({
+  params,
+}: {
+  params: {
+    formUrl: string;
+  };
+}) {
+  const form = await GetFormContentByUrl(params.formUrl);
+
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const formContent = JSON.parse(form.content) as FormElementInstance[];
+
+  return <FormSubmitComponent formUrl={params.formUrl} content={formContent} />;
+}
+
+export default SubmitPage;
