b6560f8a03f7ec9bf127a375fde654475fd33a51
Pranav M <pranavmappoli@gmail.com>
2024-03-25 21:12:59 +0530

    add side nav bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,24 @@
-import Logo from "@/src/components/Logo";
-import ThemeSwitcher from "@/src/components/ThemeSwitcher";
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
+import SidebarNav from "@/components/SidebarNav";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
-        <Logo />
-        <div className="flex gap-4 items-center">
-          <ThemeSwitcher />
-          <UserButton afterSignOutUrl="/sign-in" />
-        </div>
-      </nav>
-      <main className="flex w-full flex-grow">{children}</main>
+    <div className="flex">
+      <SidebarNav />
+
+      <div className="flex flex-col w-full min-h-screen bg-background">
+        <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+          <Logo />
+          <div className="flex items-center gap-4">
+            <ThemeSwitcher />
+            <UserButton afterSignOutUrl="/sign-in" />
+          </div>
+        </nav>
+        <main className="flex flex-grow w-full">{children}</main>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideNavItem.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { cn } from '@/lib/utils';
+import { IconType } from 'react-icons';
+
+interface SideNavItemProps {
+  title: string;
+  className?: string;
+  icon?: any;
+}
+
+const SideNavItem: FC<SideNavItemProps> = ({ title, className, icon }) => {
+  return (
+    <AccordionItem
+      value={title}
+      className="p-0 border-0 dark:text-white text-inherit"
+    >
+      <AccordionTrigger
+        className={cn(
+          'py-2.7  hover:no-underline flex items-center  text-sm ease-nav-brand my-0 mx-4  whitespace-nowrap rounded-2xl  px-4 transition-colors ',
+          className,
+        )}
+      >
+        <div className="flex items-center justify-center gap-2 text-inherit dark:text-white">
+          <div className="text-lg shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white dark:bg-black-300 dark:text-white bg-center stroke-0 text-center xl:p-1.5">
+            {icon}
+          </div>
+          <span className="text-inherit">{title}</span>
+        </div>
+      </AccordionTrigger>
+      <AccordionContent className="text-slate-500 dark:text-white">
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
+
+export default SideNavItem;
