225c02b36634e89ab5609b0a414bdc4c3f95de97
Pranav M <pranavmappoli@gmail.com>
2024-03-25 23:56:33 +0530

    code format

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -6,12 +6,13 @@ import SideNavItem from "./SideNavItem";
 import { Accordion } from "@/components/ui/accordion";
 import { BiSolidDashboard } from "react-icons/bi";
 import { IoMdCreate } from "react-icons/io";
+import ButtonBlue from "./ButtonBlue";
 
 interface SidebarNavProps {}
 
 const SidebarNav: FC<SidebarNavProps> = ({}) => {
   return (
-    <nav className="flex flex-col text-white max-w-[16rem] min-w-[16rem] pt-4 ml-5  ease-nav-brand z-990 fixed xl:sticky h-screen inset-0   w-full -translate-x-full flex-wrap   justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 -left-8 xl:left-0 xl:-translate-x-0 xl:bg-transparent ">
+    <nav className="flex flex-col text-white max-w-[16rem] min-w-[16rem] py-4 ml-5  ease-nav-brand z-990 fixed xl:sticky h-screen inset-0   w-full -translate-x-full flex-wrap   justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 -left-8 xl:left-0 xl:-translate-x-0 xl:bg-transparent ">
       <div className="flex flex-col gap-8">
         <div>
           <div className="flex px-8 py-6 m-0 text-sm whitespace-nowrap ">
@@ -44,7 +45,7 @@ const SidebarNav: FC<SidebarNavProps> = ({}) => {
 
       <div className="mx-4">
         {/* <!-- load phantom colors for card after: --> */}
-        <p className="invisible hidden text-gray-800 text-red-500 text-red-600 after:bg-gradient-to-tl after:from-gray-900 after:to-slate-800 after:from-blue-600 after:to-cyan-400 after:from-red-500 after:to-yellow-400 after:from-green-600 after:to-lime-400 after:from-red-600 after:to-rose-400 after:from-slate-600 after:to-slate-300 text-lime-500 text-cyan-500 text-slate-400 text-fuchsia-500"></p>
+
         <div
           className="after:opacity-65 after:bg-gradient-to-tl after:from-slate-600 after:to-slate-300 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']"
           sidenav-card
@@ -75,13 +76,7 @@ const SidebarNav: FC<SidebarNavProps> = ({}) => {
           </div>
         </div>
         {/* <!-- pro btn  --> */}
-        <a
-          className="inline-block w-full px-6 py-3 my-4 text-xs font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro gradient__primary hover:shadow-soft-2xl hover:scale-102"
-          target="_blank"
-          href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tailwind?ref=sidebarfree"
-        >
-          Upgrade to pro
-        </a>
+        <ButtonBlue className="" title="Upgrade to pro" />
       </div>
     </nav>
   );
