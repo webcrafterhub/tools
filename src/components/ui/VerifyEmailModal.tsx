4791b07ff2dad70ae6e3bb235ad9f37b74adeb97
Pranav M <pranavmappoli@gmail.com>
2024-04-14 10:44:59 +0530

    update back to prev version

---
+++ src/components/ui/VerifyEmailModal.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface VerifyEmailModalProps {}
+
+const VerifyEmailModal: FC<VerifyEmailModalProps> = ({}) => {
+  return <div>VerifyEmailModal</div>;
+};
+
+export default VerifyEmailModal;
