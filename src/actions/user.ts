64c32249bd575019e602f59bd3983adf1b5f4d39
Pranav M <pranavmappoli@gmail.com>
2024-04-19 14:32:10 +0530

    complete auth

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,4 +1,5 @@
 "use server";
+import bcrypt from "bcryptjs";
 
 import prisma from "@/lib/prisma";
 
@@ -34,6 +35,19 @@ export async function setEmailVerified(email: string) {
         email,
       },
       data: { emailVerified: new Date() },
+    });
+    return true;
+  } catch (error) {
+    return null;
+  }
+}
+export async function updateUserPassword(email: string, password: string) {
+  try {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: { password: bcrypt.hashSync(password, 12) },
     });
     return true;
   } catch (error) {
--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -17,8 +17,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
 // import usePasswordForgot from "@/hooks/usePasswordForgot";
 import { toast } from "./ui/use-toast";
-import { SOMETHING_WENT_WRONG } from "@/utils/contants";
+import { ERROR, LOADING, RESET_PASSWORD, SOMETHING_WENT_WRONG, SUCCESS } from "@/utils/contants";
 import { da } from "date-fns/locale";
+import { upsertResetToken } from "@/actions/auth";
+import mailer from "@/lib/mailer";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import FormError from "./FormError";
 
 interface ForgotPasswordProps {
   open: boolean;
@@ -28,7 +32,6 @@ interface ForgotPasswordProps {
 
 type FormValues = {
   email: string;
-  password: string;
 };
 
 const schema = z.object({
@@ -43,51 +46,63 @@ const ForgotPassword: FC<ForgotPasswordProps> = ({ open, dialogHandler, email })
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>({ resolver });
-  // const mutation = usePasswordForgot();
-  const onSubmit = handleSubmit((data) => console.log(data));
-  // useEffect(() => {
-  //   if (mutation.isSuccess) {
-  // toast({
-  //   title: "Error",
-  //   description: "Password reset sent successfully",
-  //   variant: "destructive",
-  // });
-  //     dialogHandler();
-  //   }
-  //   if (mutation.isError) {
-  //     toast.error(mutation.error.response?.data?.message || SOMETHING_WENT_WRONG);
-  //   }
-  // }, [mutation.isSuccess, mutation.isError]);
+  const [formStatus, setFormStatus] = useState<{ type: string; data: any }>({ type: "", data: "" });
+
+  const onSubmit = handleSubmit(async (data) => {
+    setFormStatus({ type: LOADING, data: "" });
+    const verification = await upsertResetToken(data.email);
+    if (verification.type === ERROR) {
+      setFormStatus(verification);
+      return;
+    }
+    mailer(data.email, RESET_PASSWORD, verification.data?.token);
+    setFormStatus({ type: SUCCESS, data: "" });
+  });
+
   return (
     <Dialog open={open} onOpenChange={dialogHandler}>
       <DialogContent>
-        <form onSubmit={onSubmit} noValidate>
-          <DialogHeader>
-            <DialogTitle className="text-2xl text-center">Reset password</DialogTitle>
-            <DialogDescription className="text-center">
-              You will receive an e-mail in maximum 60 seconds
-            </DialogDescription>
-          </DialogHeader>
-          <div className="my-4">
-            <input
-              {...register("email")}
-              type="email"
-              className={cn(
-                "text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow",
-                { "border-red-500": errors?.email },
-              )}
-              placeholder="Email"
-              aria-label="Email"
-              defaultValue={email || ""}
-            />
-            {errors?.email && <p className="text-sm text-red-500 dark:text-red-900">{errors.email.message}</p>}
-          </div>
-
-          <DialogFooter>
-            <ButtonDark title="Send" loading={false} />
-          </DialogFooter>
-          <p className="mt-2 text-xs text-center opacity-30">Please check your inbox/spam folder for the email.</p>
-        </form>
+        {formStatus.type !== SUCCESS && (
+          <form onSubmit={onSubmit} noValidate>
+            <DialogHeader>
+              <DialogTitle className="text-2xl text-center">Reset password</DialogTitle>
+              <DialogDescription className="text-center">
+                You will receive an e-mail in maximum 60 seconds
+              </DialogDescription>
+            </DialogHeader>
+            <div className="my-4">
+              <input
+                {...register("email")}
+                type="email"
+                className={cn(
+                  "text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow",
+                  { "border-red-500": errors?.email },
+                )}
+                placeholder="Email"
+                aria-label="Email"
+                defaultValue={email || ""}
+              />
+              {errors?.email && <p className="text-sm text-red-500 dark:text-red-900">{errors.email.message}</p>}
+            </div>
+            {formStatus.type === ERROR && <FormError message={formStatus.data} />}
+            <DialogFooter>
+              <ButtonDark title="Send" loading={formStatus.type === LOADING} />
+            </DialogFooter>
+            <p className="mt-2 text-xs text-center opacity-30">Please check your inbox/spam folder for the email.</p>
+          </form>
+        )}
+        {formStatus.type === SUCCESS && (
+          <>
+            <DialogHeader className="flex items-center">
+              <CheckCircledIcon className="w-16 h-16 text-green-500" />
+              <DialogTitle className="text-2xl text-center">Request submitted successfully</DialogTitle>
+              <DialogDescription className="text-center pt-4">
+                Please take a moment to access your email, where you'll find detailed instructions on how to reset your
+                password securely.
+              </DialogDescription>
+            </DialogHeader>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
