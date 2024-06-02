de0e99afb2fdea8979441ad139a9362393afb558
Pranav M <pranavmappoli@gmail.com>
2024-06-02 19:59:40 +0530

    fix routes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,6 @@
 export const publicRoutes = ["/"];
 export const authRoutes = ["/signin", "/signup", "/resetPassword"];
+export const privateRoutes = ["/dashboard"];
 export const defaultLoginRedirect = "/";
 export const apiAuthRoutes = "/api/auth";
 export const loginRoute = "/signin";
