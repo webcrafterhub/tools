89f89c0c3162da691b6a7cd4a8f58c996a980ece
Pranav M <pranavmappoli@gmail.com>
2024-07-06 17:33:53 +0530

    Add-error-state

--- src/components/tools/jwt decoder/JWTDecoder.tsx
+++ src/components/tools/jwt decoder/JWTDecoder.tsx
@@ -25,6 +25,7 @@ import { Input } from "@/components/ui/input";
 import { GiCancel } from "react-icons/gi";
 import LockToolTip from "@/components/LockToolTip";
 import jwt from "jwt-simple";
+import FormError from "@/components/FormError";
 interface Annotation {
   row?: number;
   column?: number;
@@ -46,7 +47,7 @@ const INITIAL_DATA = {
 function JWTDecoder() {
   const [content, setContent] = useState<string>(INITIAL_DATA.jwt);
   const [loading, setLoading] = useState(false);
-  const [validationErrors, setValidationError] = useState<string | null>("");
+  const [validationErrors, setValidationError] = useState<string | undefined>("");
   const [decodedJWT, setDecodedJWT] = useState<decodedJWTType>({});
   const [signatureVerified, setSignatureVerified] = useState<boolean>(false);
   const [decodedDisabled, setDecodedDisabled] = useState<boolean>(true);
@@ -65,7 +66,7 @@ function JWTDecoder() {
         ...decodedJWT,
         header: newHeader,
       });
-      setValidationError(null);
+      setValidationError("");
     } catch (err) {
       setValidationError("Invalid JSON in header");
     }
@@ -78,17 +79,14 @@ function JWTDecoder() {
         ...decodedJWT,
         payload: newPayload,
       });
-      setValidationError(null);
+      setValidationError("");
     } catch (err) {
       setValidationError("Invalid JSON in payload");
     }
   };
 
   const handleSignatureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateJwtToken({
-      ...decodedJWT,
-      signature: e.target.value,
-    });
+    handleSecretChange(e.target.value);
   };
 
   const handleSecretChange = async (newSecreat: string) => {
@@ -105,7 +103,7 @@ function JWTDecoder() {
     try {
       const token = jwt.encode(decodedJWT.payload, secret, "HS256", decodedJWT.header as any);
       setContent(token);
-      setValidationError(null);
+      setValidationError("");
     } catch (e) {
       setValidationError("Error while generating JWT");
     }
@@ -117,13 +115,13 @@ function JWTDecoder() {
 
   function AceChangeHandler(jwt: string) {
     try {
-      setValidationError(null);
       setContent(jwt);
     } catch (error) {}
   }
   async function decodeJWT(jwt: string) {
     try {
       const jwtDecoded = decode(jwt);
+      setValidationError("");
       setDecodedJWT(jwtDecoded);
     } catch (error) {
       //"JWT is editing"
@@ -133,82 +131,87 @@ function JWTDecoder() {
   return (
     <div className="container min-h-screen flex flex-col gap-4 justify-center">
       <FileUpload setContent={AceChangeHandler} loadingHandler={setLoading} />
-      <ResizablePanelGroup
-        direction={deviceType === "mobile" ? "vertical" : "horizontal"}
-        className="codebox md:min-h-[50vh] min-h-[100vh] md:max-h-[70vh] "
-      >
-        <ResizablePanel className="relative">
-          <Card className="h-full shadow-inner shadow-blue-600 overflow-y-scroll scrollbar-none scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-blue-400 scrollbar-track-slate-300">
-            {loading && <LoadingGear />}
-            {!loading && (
-              <>
-                <CardHeader className="flex flex-row justify-between items-center">
-                  <div className="flex flex-col gap-2">
-                    <CardTitle>JWT EDITOR</CardTitle>
-                    <CardDescription>Copy and paste your JWT data here</CardDescription>
-                  </div>
-                  <div className="flex gap-2">
-                    <ClearToolTip clearHandler={() => AceChangeHandler("")} disabled={loading} />
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <AceJWTEditor
-                    value={content}
-                    changeHandler={AceChangeHandler}
-                    validationHandler={validationHandler}
-                    placeholder="Paste or type your content here"
-                    focus={true}
-                  />
-                </CardContent>
-              </>
-            )}
-          </Card>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel>
-          <Card className="h-full flex flex-col relative  shadow-inner shadow-green-600 ">
-            {loading && <LoadingGear />}
-            {!loading && (
-              <>
-                <CardHeader className="flex flex-row justify-between items-center">
-                  <div className="flex flex-col gap-2">
-                    <CardTitle>DECODED DATA</CardTitle>
-                    <CardDescription>View your JWT decoded data here</CardDescription>
-                  </div>
-                  <div className="flex gap-2">
-                    <CopyToolTip data={content} disabled={loading || !!validationErrors} />
-                    <LockToolTip locked={decodedDisabled} lockHandler={() => setDecodedDisabled((prev) => !prev)} />
-                  </div>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-3 h-full overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-green-400 scrollbar-track-slate-300">
-                  <DecodedTextBox
-                    type="Header"
-                    value={JSON.stringify(decodedJWT.header, null, 2)}
-                    color="text-pink-500"
-                    disabled={decodedDisabled}
-                    changeHandler={handleHeaderChange}
-                  />
-                  <DecodedTextBox
-                    type="Payload"
-                    value={JSON.stringify(decodedJWT.payload, null, 2)}
-                    color="text-blue-500"
-                    disabled={decodedDisabled}
-                    changeHandler={handlePayloadChange}
-                  />
-                  <DecodedTextBox
-                    type="Signature"
-                    value={String(decodedJWT.signature || "")}
-                    color="text-red-500"
-                    disabled={decodedDisabled}
-                    changeHandler={handleSignatureChange}
-                  />
-                </CardContent>
-              </>
-            )}
-          </Card>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-      <div className="flex justify-between items-center backdrop-blur-md bg-gray-100 dark:bg-white/5 px-4 py-2 rounded-lg">
+      <div className="relative">
+        <ResizablePanelGroup
+          direction={deviceType === "mobile" ? "vertical" : "horizontal"}
+          className="codebox md:min-h-[50vh] min-h-[100vh] md:max-h-[70vh] "
+        >
+          <ResizablePanel className="relative">
+            <Card className="h-full shadow-inner shadow-blue-600 overflow-y-scroll scrollbar-none scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-blue-400 scrollbar-track-slate-300">
+              {loading && <LoadingGear />}
+              {!loading && (
+                <>
+                  <CardHeader className="flex flex-row justify-between items-center">
+                    <div className="flex flex-col gap-2">
+                      <CardTitle>JWT EDITOR</CardTitle>
+                      <CardDescription>Copy and paste your JWT data here</CardDescription>
+                    </div>
+                    <div className="flex gap-2">
+                      <ClearToolTip clearHandler={() => AceChangeHandler("")} disabled={loading} />
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <AceJWTEditor
+                      value={content}
+                      changeHandler={AceChangeHandler}
+                      validationHandler={validationHandler}
+                      placeholder="Paste or type your content here"
+                      focus={true}
+                    />
+                  </CardContent>
+                </>
+              )}
+            </Card>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel>
+            <Card className="h-full flex flex-col relative  shadow-inner shadow-green-600 ">
+              {loading && <LoadingGear />}
+              {!loading && (
+                <>
+                  <CardHeader className="flex flex-row justify-between items-center">
+                    <div className="flex flex-col gap-2">
+                      <CardTitle>DECODED DATA</CardTitle>
+                      <CardDescription>View your JWT decoded data here</CardDescription>
+                    </div>
+                    <div className="flex gap-2">
+                      <CopyToolTip data={content} disabled={loading || !!validationErrors} />
+                      <LockToolTip locked={decodedDisabled} lockHandler={() => setDecodedDisabled((prev) => !prev)} />
+                    </div>
+                  </CardHeader>
+                  <CardContent className="flex flex-col gap-3 h-full overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-green-400 scrollbar-track-slate-300">
+                    <DecodedTextBox
+                      type="Header"
+                      value={JSON.stringify(decodedJWT.header, null, 2)}
+                      color="text-pink-500"
+                      disabled={decodedDisabled}
+                      changeHandler={handleHeaderChange}
+                    />
+                    <DecodedTextBox
+                      type="Payload"
+                      value={JSON.stringify(decodedJWT.payload, null, 2)}
+                      color="text-blue-500"
+                      disabled={decodedDisabled}
+                      changeHandler={handlePayloadChange}
+                    />
+                    <DecodedTextBox
+                      type="Signature"
+                      value={String(decodedJWT.signature || "")}
+                      color="text-red-500"
+                      disabled={decodedDisabled}
+                      changeHandler={handleSignatureChange}
+                    />
+                  </CardContent>
+                </>
+              )}
+            </Card>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+        {validationErrors && (
+          <FormError message={validationErrors} className="absolute bottom-0 w-full translate-y-full -m-1 z-50" />
+        )}
+      </div>
+      <div className="mt-9 flex justify-between items-center backdrop-blur-md bg-gray-100 dark:bg-white/5 px-4 py-2 rounded-lg">
         <div className="flex flex-col gap-2">
           <Label className="text-sm font-semibold capitalize" htmlFor="token">
             Verify JWT Signature
