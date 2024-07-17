e394139840f7e9a65e7d302f9d11b712478ef9fe
Pranav M <pranavmappoli@gmail.com>
2024-07-17 14:56:40 +0530

    change github url

--- src/utils/siteConfig.ts
+++ src/utils/siteConfig.ts
@@ -1,5 +1,5 @@
 const links = {
   twitter: "https://x.com/webcrafterhub",
-  github: "https://github.com/webcrafterhub/coming_soon",
+  github: "https://github.com/webcrafterhub/tools",
 };
 export { links };
