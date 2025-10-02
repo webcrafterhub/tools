68befc5f351d8dd0c2e5bc5eda0c10273553c18c
Pranav M <pranavmappoli@gmail.com>
2025-10-02 10:33:46 +0530

    fix assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  assetPrefix: "/tools",
   webpack(config) {
     config.resolve.fallback = {
+    
       // if you miss it, all the other options in fallback, specified
       // by next.js will be dropped.
       ...config.resolve.fallback,
